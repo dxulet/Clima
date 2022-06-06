ebd161a7d198920eaa56b0f20f29e3dc19ff8b49
Daulet Ashikbayev <dxulet@gmail.com>
2022-06-06 21:38:51 +0500

    finished

---
+++ Clima/WeatherData.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Daulet Ashikbayev on 04.06.2022.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Clima/WeatherManager.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Daulet Ashikbayev on 04.06.2022.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
